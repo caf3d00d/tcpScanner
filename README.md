fe68a191ff234ba77779973cdb23a53c980fb067
unixity <leetpwnsroot@gmail.com>
2022-07-30 01:42:50 +0200

    Change output and add README

---
+++ README.md
@@ -0,0 +1,38 @@
+# tcpScanner
+
+**A simple network scanning written in go**
+
+### Why?
+    * well, educational purpose maybe.
+
+### Usage:
+Build the package:
+```shell
+go build
+```
+
+Scanning opened ports:
+```shell
+./tcpScanner hostname from to
+```
+
+Example:
+```shell
+./tcpScanner hostname <range of ports>
+./tcpScanner scanme.nmap.org 21 81
+```
+
+### Attention
+
+This script is under active maintain, although it is not perfect.
+There might be unexpected behaviour as it would grow up, so feel free to contribute,
+I'm open to learn.
+
+### TODO
+* Enhance the performance using goroutine technique.
+* Add more scanning technique.
+* Fix packet loosing issue, maybe?
+* Refactor the code.
+* Write more TODO.
+
+[![forthebadge](https://forthebadge.com/images/badges/made-with-go.svg)](https://forthebadge.com)[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com)[![forthebadge](https://forthebadge.com/images/badges/works-on-my-machine.svg)](https://forthebadge.com)
--- main.go
+++ main.go
@@ -31,22 +31,38 @@ func handleArgs(Args []string) (string, int, int) {
 
 func main() {
 	host, rf, rt := handleArgs(os.Args)
+	var openedPorts []string
 
 	for rf <= rt {
 		address := fmt.Sprintf("%s:%d", host, rf)
-		//fmt.Printf("rf -> %d\n", rf)
+		// Didn't know port 0 is invalid
+		if rf == 0 {
+			rf++
+			continue
+		}
 		connection, err := net.Dial("tcp", address)
 
 		if err != nil {
+			fmt.Printf("\033[0;31m* %s -> closed\n\u001B[0;37m", address)
 			rf++
 			continue
 		}
 		err = connection.Close()
 		if err != nil {
-			log.Fatal("Couldn't close a connection")
+			log.Fatal("\u001B[0;31mCouldn't close a connection\u001B[0;37m")
 		}
-		fmt.Printf("found open port at: %s\n", address)
+		fmt.Printf("\033[0;32m* %s -> opened\n\u001B[0;37m", address)
+		openedPorts = append(openedPorts, address)
 		rf++
 	}
-
+	i := 0
+	if len(openedPorts) > 0 {
+		fmt.Printf("\u001B[0;32m\n\nSummary, Opened ports:\n")
+		for range openedPorts {
+			fmt.Printf("%d. %s\n\033[0;37m", i+1, openedPorts[i])
+			i++
+		}
+	} else {
+		fmt.Printf("\033[0;31m\n\nSummary, no opened port found\n\u001B[0;37m")
+	}
 }
